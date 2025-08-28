4ded0bc76129a8701244ac839225d473215f8deb
DSM2 <fatec-dsm2@academico.intra>
2025-08-28 19:32:14 -0300

    @ArthurParra

---
+++ Atividade-Arrays e Objetos-JS/Objetos_Gerenciamento_Clientes/script.js
@@ -0,0 +1,33 @@
+const pessoa = {
+  constructor(nome, endereco, cpf) {
+    this.nome = nome;
+    this.endereco = endereco;
+    this.cpf = cpf;
+  },
+};
+const ListaClientes = [
+  {
+    nome: "João Vitor",
+    endereco: "Rua Vereador Aércio, 123",
+    cpf: 12345678901,
+  },
+  {
+    nome: "Elias Pinheiro",
+    endereco: "Rua Nadador Franco, 456",
+    cpf: 23456789012,
+  },
+  {
+    nome: "Myrella Castellani",
+    endereco: "Rua Reverendo Silva, 789",
+    cpf: 34567890123,
+  },
+];
+ListaClientes.forEach((pessoa) => {
+  document.write(`
+        <p>
+            Nome do Cliente: ${pessoa.nome}<br>
+            Endereço: ${pessoa.endereco}<br>
+            CPF: ${pessoa.cpf}<br>
+        </p>
+        `);
+});
