68de0aa8554dedfb8da3c296dd7d01d878624d5e
DSM2 <fatec-dsm2@academico.intra>
2025-09-05 22:05:51 -0300

    @ArthurParra

--- Atividade-Arrays e Objetos-JS/Objetos_Gerenciamento_Clientes/script.js
+++ Atividade-Arrays e Objetos-JS/Objetos_Gerenciamento_Clientes/script.js
@@ -1,6 +1,3 @@
- HEAD
-//criando uma variavel atribuido a um objeto
- 
 const pessoa = {
   constructor(nome, endereco, cpf) {
     this.nome = nome;
@@ -25,6 +22,43 @@ const ListaClientes = [
     cpf: 34567890123,
   },
 ];
+document.write(`<h2>Esta é a nossa lista de clientes: </h2>`)
+ListaClientes.forEach((pessoa) => {
+  document.write(`
+        <p>
+            Nome do Cliente: ${pessoa.nome}<br>
+            Endereço: ${pessoa.endereco}<br>
+            CPF: ${pessoa.cpf}<br><br>
+        </p>
+        `);
+});
+
+ListaClientes.push({
+  nome: "Mike Star",
+  endereco: "Rua Mario Games da Silva, 012",
+  cpf: 27439190493,
+});
+
+document.write(`<h2>Esta é a nova lista de Clientes: </h2>`)
+
+ListaClientes.forEach((pessoa) => {
+  document.write(`
+        <p>
+            Nome do Cliente: ${pessoa.nome}<br>
+            Endereço: ${pessoa.endereco}<br>
+            CPF: ${pessoa.cpf}<br><br>
+        </p>
+        `);
+});
+
+ListaClientes.unshift({
+  nome: "Layne Stanley",
+  endereco: "Rua Calçada do Rock, 234",
+  cpf: 28402639052,
+});
+
+document.write(`<h2>Esta é a nova lista de Clientes: </h2>`)
+
 ListaClientes.forEach((pessoa) => {
   document.write(`
         <p>
