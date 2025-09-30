002535de99ccd0090b3840e8dbb4729d07f51a12
DSM2 <fatec-dsm2@academico.intra>
2025-09-29 22:09:22 -0300

    Atividade Gerenciamento_Estoque

---
+++ Atividade-Gerenciamento-Estoque/Gerenciamente_Estoque/src/main/java/com/fatec/gerenciamente_estoque/Gerenciamente_Estoque.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fatec.gerenciamente_estoque;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class Gerenciamente_Estoque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
